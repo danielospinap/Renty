56d7ebc21d0cb41035dcab1b4842a4e15629d4c7
Jamon <daniel.ospinaperez@gmail.com>
2018-11-15 16:33:56 -0500

    api basica

---
+++ src/main/scala/mongodb/package.scala
@@ -0,0 +1,17 @@
+package mongodb
+
+import com.typesafe.config.ConfigFactory
+import org.bson.codecs.configuration.CodecRegistries._
+import org.mongodb.scala._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.mongodb.scala.bson.codecs.Macros._
+
+import models._
+
+object Mongo {
+  lazy val config = ConfigFactory.load()
+  lazy val mongoClient: MongoClient = MongoClient("mongodb://rentyadmin:renty123@ds037067.mlab.com:37067/renty")
+  lazy val codecRegistry = fromRegistries(fromProviders(classOf[Car]), DEFAULT_CODEC_REGISTRY)
+  lazy val database: MongoDatabase = mongoClient.getDatabase("renty").withCodecRegistry(codecRegistry)
+  lazy val carCollection: MongoCollection[Car] = database.getCollection[Car]("cars")
+}
